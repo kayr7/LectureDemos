db3866fe7ba16c4a1d5edc51556cab2a208465d8
kayr7 <54975496+kayr7@users.noreply.github.com>
2024-02-24 15:20:38 +0100

    simple recording demo

---
+++ my-lecture-demos/pages/demos/demo1.js
@@ -0,0 +1,10 @@
+import Recorder from './recorder/Recorder'; // Adjust the path as necessary
+
+export default function LecturePage() {
+  return (
+    <div>
+      <h1>Lecture Audio Recorder</h1>
+      <Recorder />
+    </div>
+  );
+}
